6adf04404c68e70254157e9a90f501acbf284a59
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2023-10-01 20:52:03 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    int[] a = new int[] { 5, 8, 1, 5, 9, 2 };
+
+    int m = findMax(a);    
+    System.out.println(m);
+  }
+
+  public int findMax(int[] a) {
+    return findMaxR(a, a.length - 1);
+  }
+  
+  public int findMaxR(int[] a, int n) {
+    return a[0];
   }
 }
